77964cd02fc3e159290271a5212e0bc6f6e85992
Vũ Thanh Hải <haithanhvu0501@gmail.com>
2025-10-18 09:33:48 +0700

    Update delete fontAwesome Package

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import '@fortawesome/fontawesome-free/css/all.min.css';
 import AppRouter from "./routes/AppRouter";
 import { UserProvider } from "./context/UserContext";
 
